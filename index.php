4a55a01dc2f9733f0654103a291286dd29fc9920
jtrovato <josephjtrovato@gmail.com>
2013-04-05 16:02:51 -0400

    updating orginal stuff

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
    echo exec('python counter.py');
+   $eng_count = exec('python get_eng.py');
    $hits = exec('python hits.py');
-   echo $hits;
+   echo "Total Vistors: $hits ";
+   $ip = $_SERVER['REMOTE_ADDR'];
+   $ip_head = substr($ip, 0, 7);
+   if ($ip_head == "158.130")
+   	$eng_count = exec('python eng_count.py');
+   echo "Visitors from Penn Engineering: $eng_count";
+
 ?>
